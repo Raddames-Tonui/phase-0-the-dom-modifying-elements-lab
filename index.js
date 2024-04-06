22197965a01ad5fc0f79e4d25eb808a601c5a3bc
Raddames <raddamestonui48@gmail.com>
2024-04-07 01:58:36 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+    // Write your code here!
+    const mainElement = document.getElementById("main").remove()
+
+    const newHeader = document.createElement("h1")
+
+    newHeader.id = "victory"
+
+    newHeader.textContent = "RADDAMES TONUI is the champion"
+    document.body.appendChild(newHeader)
